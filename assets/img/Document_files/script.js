727a5858243b2d7ac4e6b3d6e213b42edc306b3c
Hakan Tekin <hak4n45@gmail.com>
2023-12-28 18:04:17 +0300

    ufak box shadow düzeltmesi

---
+++ assets/img/Document_files/script.js
@@ -0,0 +1,35 @@
+const form = document.querySelector("#form");
+const dailyNote = document.querySelector("#dailyNote");
+const container = document.querySelector(".container");
+const box = document.querySelector(".box");
+let dailyNotes = [];
+if(localStorage.dailyNotes){
+    dailyNotes = JSON.parse(localStorage.dailyNotes)
+}
+form.addEventListener("submit", function(e){
+    e.preventDefault();
+    const nowDate = new Date();
+    const newDate = nowDate.toLocaleString("tr")
+
+
+    dailyNotes.push({
+        note: dailyNote.value,
+        created_at: newDate
+    });
+    dailyNote.value = "";
+    localStorage.dailyNotes = JSON.stringify(dailyNotes)
+    getDailyNotes();
+})
+
+
+
+function getDailyNotes(){
+    container.innerHTML = "";
+    for (const dailyNote of dailyNotes.reverse()){
+        container.innerHTML +=  `⁠<div class="box">${dailyNote.note} - ${dailyNote.created_at}</div>
+        `
+    }
+}
+
+
+getDailyNotes();
